a4a733069eb15b433324ad13952e74cbf131ce8a
Danilo Mancini <d.manciniqueiroz@gmail.com>
2021-11-23 12:33:35 -0300

    Adicionar movimento e gravidade
    Adicionar controles de movimento básicos, um pulo e gravidade. Todos os códigos estão comentados nas partes mais essenciais.

--- TCM Project/Assets/Scripts/MouseLook.cs
+++ TCM Project/Assets/Scripts/MouseLook.cs
@@ -6,26 +6,31 @@ public class MouseLook : MonoBehaviour
 {
     public Transform playerBody;
 
+    // Sensitividade do mouse
     public float mouseSensitivity = 100f;
 
     float xRotation = 0f;
 
     void Start()
     {
+        // Prende o cursor no centro da tela e o torna invisível enquanto o jogo está rodando
         Cursor.lockState = CursorLockMode.Locked;
-
     }
 
     
     void Update()
     {
+        // Adquire as informações de cada eixo de movimento
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Implementação para a rotação vertical da câmera 
+        // (clamp é usado para que o jogador não possa virar para trás mais do que o natural)
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90, 90);
-
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        // Implementação da rotação horizontal da câmera
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
---
+++ TCM Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float movementSpeed;
+    public float gravity;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    // Define a distância dos pés do jogador na qual o chão é detectado
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Update()
+    {
+         // Verifica se o jogador está no chão
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Anula a velocidade do jogador se ele encostar no chão
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        // Movimento básico com WASD
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 movement = transform.right * x + transform.forward * z;
+        controller.Move(movement * movementSpeed * Time.deltaTime);
+
+        // Implementação simples de um pulo
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        }
+
+        // Aqui o Time.deltaTime é multiplicado novamente devido à equação geral da gravidade
+        velocity.y += gravity * Time.deltaTime;
+
+        // Movimenta o jogador com todas as implementações juntas
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
